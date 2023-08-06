5b50c42d8cca0faad57ef4659809ab5a1234183c
Replit user <>
2023-08-06 06:54:41 +0530

    inital commit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var app = express();
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
 app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
-
+  
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
@@ -21,16 +21,20 @@ app.get("/", function(req, res) {
 
 // your first API endpoint... 
 app.get("/api/:date", function(req, res) {
+  //The colon before date means that it's a variable that stores whatever input the user gives.
   const dateString = req.params.date // accessing the input which the user gives
   const dateStringRegex = /^[0-9]+$/
   const numbersOnly = dateStringRegex.test(dateString);
-  //we used regex /^[0-9]+$/ to test the dateString from the user. If the test returns true, it means the user provided a timestamp and if the test returns false, it means the user provided a date.
+  //dateStringRegex is a regular expression which contains all the possible strings that contains [0-9].
+  //if the string provided by the user(dateString) matches the regular expression(dateStringRegex) then it returns true else false. means for timestamp it returns true and for date it returns fasle.
 
   if (!numbersOnly) { // means the user provided a date
     const unixTimestamp = Date.parse(dateString); 
+    //Date.parse() method is used to convert a date string into a Unix timestamp
     // If the user provided a valid date, this will return a Unix timestamp and for invalid date it returns NAN
     const utcDate = new Date(unixTimestamp).toUTCString();
     //Once you have the unixTimestamp, you can pass it as a parameter to the Date object to get the actual date.
+    //The toUTCString() method is useful for getting a standardized, human-readable representation of dates and time
 
     unixTimestamp
       ? res.json({ "unix": unixTimestamp, "utc": utcDate })
@@ -41,6 +45,7 @@ app.get("/api/:date", function(req, res) {
 
   else { // if the user provided timestamp instead
     const unixTimestamp = parseInt(dateString)
+    //the user already provided the Unix timestamp. So you just use parseInt() to convert it from a string to a number.
     const actualDate = new Date(unixTimestamp)
     const utcDate = actualDate.toUTCString()
     //.toUTCString() converts date to a more human-readable format
@@ -52,6 +57,7 @@ app.get("/api/:date", function(req, res) {
 
 //Requests with empty date parameter ie /api/ -- it should return current date and UNIX timestamp
 app.get("/api/", (req, res) => {
+  //for today's date and time
   let date = new Date();
   let UTC = date.toUTCString();
   let UNIX = date.getTime();
